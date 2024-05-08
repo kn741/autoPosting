ada852d5bca65752ae2552851270fa284e0633b9
Chien Yang <kanzaki741@gmail.com>
2024-05-08 20:19:25 +0800

    UI&FB login

--- autoPosting.py
+++ autoPosting.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+import postUi,sys
+from PyQt6 import QtWidgets
+
+app = QtWidgets.QApplication(sys.argv)
+PlatformPoster = QtWidgets.QMainWindow()
+ui = postUi.Ui_PlatformPoster()
+ui.setupUi(PlatformPoster)
+PlatformPoster.show()
+sys.exit(app.exec())
+
--- fbPost.py
+++ fbPost.py
@@ -1,56 +1,60 @@
-account = "urAccount"
-password = "urPassword"
-
-def getSettings():
-    
-    postingPath = {}
-    postingPath["私人"] = "/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/form/div/div[2]/div/div/div[2]/div/div/div[1]/div/div/div[2]/div/div[6]/div/div/div[2]/div[1]/div/div"
-    postingPath["朋友"] = "/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/form/div/div[2]/div/div/div[2]/div/div/div[1]/div/div/div[2]/div/div[2]/div/div[1]/div[2]/div[1]/div/div/div[1]/span"
-    postingPath["所有人"] = "/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/form/div/div[2]/div/div/div[2]/div/div/div[1]/div/div/div[2]/div/div[1]/div/div[1]/div[2]"
-    settings = {}
-    settings["account"] = account
-    settings["password"] = password
-    settings["postingSettings"] = postingPath["私人"]
-    settings["postingMessage"] = "Hello, this is a test message."
-    return settings
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
-settings = getSettings()
-driver = webdriver.Chrome()
-options = Options()
-prefs = {"profile.default_content_setting_values.notifications": 2}
-options.add_experimental_option("prefs", prefs)
-options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(options=options)
-driver.get("https://www.facebook.com/")
 
-elementPath = ["/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div/div[3]/div/div[2]/div/div/div/div[1]/div/div[1]/span"
-               ,"/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/form/div/div[1]/div/div/div/div[1]/div[2]/div[2]/div[2]/div/div/div/div/span/div/div/div/span"
-               ,settings["postingSettings"]
-               ,"/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/form/div/div[2]/div/div/div[2]/div/div/div[2]/div[2]/div[2]/div/div/div[1]/div/span/span"
-               ]
+class fbLoginModule:
+    def __init__(self,account,password,postingSettings):
+        self.settings = self.getSettings(account,password,postingSettings)
+        
+    def getSettings(self,account,password,postingSettings):
+        postingPath = {}
+        postingPath["私人"] = "/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/form/div/div[2]/div/div/div[2]/div/div/div[1]/div/div/div[2]/div/div[5]/div/div[1]/div[2]/div[1]/div/div/div/span"
+        postingPath["朋友"] = "/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/form/div/div[2]/div/div/div[2]/div/div/div[1]/div/div/div[2]/div/div[2]/div/div[1]/div[2]/div[1]/div/div/div[1]/span"
+        postingPath["公開"] = "/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/form/div/div[2]/div/div/div[2]/div/div/div[1]/div/div/div[2]/div/div[1]/div/div[1]/div[2]"
+        settings = {}
+        settings["account"] = account
+        settings["password"] = password
+        settings["postingSettings"] = postingPath[postingSettings]
+        settings["postingMessage"] = "Hello, this is a test message."
+        return settings
 
-loginElement = driver.find_element(By.ID, "email")
-loginElement.send_keys(account)
-loginElement = driver.find_element(By.ID, "pass")
-loginElement.send_keys(password)
-loginElement = driver.find_element(By.NAME, "login")
-loginElement.click()
+    def login(self):
+        settings = self.settings
+        driver = webdriver.Chrome()
+        options = Options()
+        prefs = {"profile.default_content_setting_values.notifications": 2}
+        options.add_experimental_option("prefs", prefs)
+        options.add_experimental_option("detach", True)
+        driver = webdriver.Chrome(options=options)
+        driver.get("https://www.facebook.com/")
 
-for i in elementPath:
-    postingElement = WebDriverWait(driver, 10).until(lambda d: d.find_element(By.XPATH, i))
-    postingElement.click()
+        elementPath = ["/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div/div[3]/div/div[2]/div/div/div/div[1]/div/div[1]/span"
+                ,"/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/form/div/div[1]/div/div/div/div[1]/div[2]/div[2]/div[2]/div/div/div/div/span/div/div/div/span"
+                ,settings["postingSettings"]
+                ,"/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/form/div/div[2]/div/div/div[2]/div/div/div[2]/div[2]/div[2]/div/div/div[1]/div/span/span"
+                ]
 
-postingElement = WebDriverWait(driver, 10).until(lambda d: d.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/form/div/div[1]/div/div/div/div[2]/div[1]/div[1]/div[1]/div/div/div[1]"))
-postingElement.send_keys(settings["postingMessage"])
-
-
-time.sleep(3)
-
-postingElement = WebDriverWait(driver, 10).until(lambda d: d.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/form/div/div[1]/div/div/div/div[3]/div[2]/div/div/div[1]/div/span/span"))
-postingElement.click()
-
+        loginElement = driver.find_element(By.ID, "email")
+        loginElement.send_keys(settings["account"])
+        loginElement = driver.find_element(By.ID, "pass")
+        loginElement.send_keys(settings["password"])
+        loginElement = driver.find_element(By.NAME, "login")
+        loginElement.click()
+        
+        try:
+            for i in elementPath:
+                postingElement = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, i)))
+                postingElement.click()
+        except:
+            print("exception occurred.")
+            driver.quit()
+            return False
+        postingElement = WebDriverWait(driver, 10).until(lambda d: d.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/form/div/div[1]/div/div/div/div[2]/div[1]/div[1]/div[1]/div/div/div[1]"))
+        postingElement.send_keys(settings["postingMessage"])
+        time.sleep(3)
+        postingElement = WebDriverWait(driver, 10).until(lambda d: d.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/form/div/div[1]/div/div/div/div[3]/div[2]/div/div/div[1]/div/span/span"))
+        # postingElement.click()
